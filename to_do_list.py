ae2cbf18ffeaa0fbe951da278b38fc81a084c061
Heidy <heidy1443@hotmail.com>
2025-07-23 18:45:34 +0100

    save list to .json

--- to_do_list.py
+++ to_do_list.py
@@ -1,16 +1,13 @@
 print("Welcome to my To-Do List!")
 import json
-todo=[]
-y=json.dumps(todo)
 
-with open('todo.txt', 'r') as json_file:
+todo=[]
+
+with open('todo.json', 'r') as json_file:
    todo = json.load(json_file)
-   
+
 def todo_():
-   
-   with open('todo.txt', 'w') as json_file:
-      json.dump(todo, json_file)
-   
+
    while True:
       task=input("What would you like to do? 1. View tasks 2. Add a task 3. Remove a task 4. Exit")
       
@@ -26,9 +23,13 @@ def todo_():
       if task=="3":
             index=int(input("Enter the number of the task to remove:"))
             todo.pop(index-1)
+            print("Task removed!")
       
       if task=="4":
+         with open('todo.json', 'w') as json_file:
+               json.dump(todo, json_file, indent=2)
          print("Goodbye!")
+         
          break
 
 todo_()

997dcbc475f894614a78745a3da58120a17c2006
Heidy <heidy1443@hotmail.com>
2025-08-01 21:03:31 +0100

    homework

--- cs50p/outdated/outdated.py
+++ cs50p/outdated/outdated.py
@@ -13,29 +13,43 @@ months=[
     "December"
 ]
 
-date=input()
-
-if any (c.isalpha for c in date):
-    mondate,year=date.split(",")
-    mon,day=mondate.split()
-    year=int(year)
-    mon=str(mon)
-    day=int(day)
-    
-    for i, value in enumerate(months):
-        if mon == value:
-            m = i+1
+while True:
+    try:
+        date=input("date:")
         
-    if day<31:
-        print(f"{year}-{m:02}-{day:02}")
+        if any(c.isalpha() for c in date):
+            mondate,year=date.split(",")
+            mon,day=mondate.split()
+            year=int(year)
+            mon=str(mon)
+            day=int(day)
+    
+            for i, value in enumerate(months):
+                if mon == value:
+                    m = i+1
+                
+            if day>31:
+                pass
+            
+            else:
+                print(f"{year}-{m:02}-{day:02}")
+                break
 
-else:
-    x,y,z=date.split("/")
-    x=int(x)
-    y=int(y)
-    z=int(z)
-    if y<31:
-        print(f"{z}-{x:02}-{y:02}")
+        else:
+            x,y,z=date.split("/")
+            x=int(x)
+            y=int(y)
+            z=int(z)
+            
+            if x>12 and y>31:
+                pass
+            
+            else:
+                print(f"{z}-{x:02}-{y:02}")
+                break
+        
+    except ValueError:
+        pass
     
     
     
